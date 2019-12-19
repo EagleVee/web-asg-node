e0b9bb5c2fd70b3fa5f4da8219a51900b8a68936
EagleVee <nnavu1@gmail.com>
2019-12-19 13:20:44 +0700

    fix index

--- Config/index.js
+++ Config/index.js
@@ -1,6 +1,6 @@
-export const MONGO_CONNECTION_STRING = "mongodb://localhost:27017/figello";
+export const MONGO_CONNECTION_STRING = "mongodb://localhost:27017/web-asg";
 export const MONGO_ATLAS_CONNECTION_STRING =
-  "mongodb+srv://admin:dafug123456@cluster0-3aw3a.mongodb.net/figello";
+  "mongodb+srv://admin:dafug123456@cluster0-3aw3a.mongodb.net/web-asg";
 export const SECRET_KEY = 10;
-export const JWT_SECRET = "nnavu.figello.task";
+export const JWT_SECRET = "nnavu.web.asg";
 export const TOKEN_EXPIRE_MILLISECOND = 604800000;
